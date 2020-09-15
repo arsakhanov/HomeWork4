d265d3abe05e833e61fbce7b89ce57f2531bc728
arsakhanov <baki_12_95@mail.ru>
2020-09-15 10:59:31 +0300

    +interface Sort with method sort

---
+++ src/ru/arsakhanov/Sort.java
@@ -0,0 +1,14 @@
+package ru.arsakhanov;
+
+/**
+ * интерфейс Sort
+ */
+public interface Sort {
+    /**
+     * метод сортировки
+     *
+     * @param persons массив объектов типа Person
+     */
+    static void sort(Person[] persons) {
+    }
+}
