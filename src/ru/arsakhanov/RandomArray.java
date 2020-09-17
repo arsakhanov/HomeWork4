44fdef09f80b0f15393ebed91817971353a003f0
arsakhanov <baki_12_95@mail.ru>
2020-09-17 22:26:51 +0300

    +JavaDoc

--- src/ru/arsakhanov/RandomArray.java
+++ src/ru/arsakhanov/RandomArray.java
@@ -1,9 +1,16 @@
 package ru.arsakhanov;
 
+/**
+ * класс для генерирования рандомных объектов для массива типа Person
+ */
 public class RandomArray {
 
-    private final int COUNT = 100;
+    private final int COUNT = 100; //количество сгенерированных объектов
 
+    /**
+     * метод генерирует рандомные объекты для Person и возвращает массив
+     * @return возвращает массив типа Person[]
+     */
     public Person[] createArrayRandom(){
         Person[] persons = new Person[COUNT];
         for (int i = 0; i < persons.length; i++) {
--- src/ru/arsakhanov/ShowPersons.java
+++ src/ru/arsakhanov/ShowPersons.java
@@ -1,8 +1,17 @@
 package ru.arsakhanov;
 
+
+/**
+ * Класс для создания методов вывода на экран массива Person
+ */
 public class ShowPersons {
 
-    public void showArray(Person[] people){
+    /**
+     * метод для вывода всех элементов массива Person на экран
+     *
+     * @param people массива типа Person[]
+     */
+    public void showArray(Person[] people) {
         for (int i = 0; i < people.length; i++) {
             System.out.println(people[i]);
         }
