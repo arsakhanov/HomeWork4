737850d9ce15e9671a87b85e3393d1f288fdb251
arsakhanov <baki_12_95@mail.ru>
2020-09-17 20:32:44 +0300

    -поля в исключении

--- src/ru/arsakhanov/exceptions/SameNameAndAgeException.java
+++ src/ru/arsakhanov/exceptions/SameNameAndAgeException.java
@@ -6,27 +6,14 @@ package ru.arsakhanov.exceptions;
  * генерирует одинаковые age и name у нескольких объектов
  */
 public class SameNameAndAgeException extends NullPointerException {
-    private int age;
-    private String name;
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getName() {
-        return name;
-    }
 
     /**
      * Пользовательсое исключение для компаратора класса Person
      *
      * @param message строка типа String
-     * @param age     возраст типа int
-     * @param name    имя типа String
      */
-    public SameNameAndAgeException(String message, int age, String name) {
+    public SameNameAndAgeException(String message) {
         super(message);
-        this.age = age;
-        this.name = name;
+
     }
 }
