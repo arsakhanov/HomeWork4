32f8a4f522be3f83fb73164b265d8e5af67419b3
arsakhanov <baki_12_95@mail.ru>
2020-09-17 21:45:13 +0300

    +класс для вывода на консоль массива Person[]

---
+++ src/ru/arsakhanov/ShowPersons.java
@@ -0,0 +1,10 @@
+package ru.arsakhanov;
+
+public class ShowPersons {
+
+    public void showArray(Person[] people){
+        for (int i = 0; i < people.length; i++) {
+            System.out.println(people[i]);
+        }
+    }
+}
